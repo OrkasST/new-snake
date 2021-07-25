09b1b7c331aa9926eaf1fb5f6a8b88f0712070e7
OrkasST <egor31@outlook.com>
2021-07-25 20:48:22 +0300

    start menu; mobilizer

--- scripts/start-menu.js
+++ scripts/start-menu.js
@@ -1,20 +1,14 @@
-let startMenu = {
-    startBtn: {
-        x: 50,
-        y: 50,
-        width: 200,
-        height: 200
-    }
+let startBtn = {
+    x: 50,
+    y: 50,
+    width: 200,
+    height: 200
 }
 
 function displayMenu() {
-    for(let i in startMenu) {
-        if (startMenu.hasOwnProperty(i)) {
-            ctx.beginPath();
-            ctx.fillRect(i.x, i.y, i.width, i.height);
-            ctx.closePath();
-        }
-    }
+    ctx.beginPath();
+    ctx.fillRect(startBtn.x, startBtn.y, startBtn.width, startBtn.height);
+    ctx.closePath();
 }
 
 
